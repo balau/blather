0b9a3602595fdf65319b5e831251f448898f39a5
Francesco Balducci <balau@users.sourceforge.net>
2017-01-21 21:38:37 +0100

    Port to GStreamer 1.0, use gi

--- Recognizer.py
+++ Recognizer.py
@@ -2,23 +2,24 @@
 # -- this code is licensed GPLv3
 # Copyright 2013 Jezra
 
-import pygst
-pygst.require('0.10')
-import gst
 import os.path
-import gobject
 import sys
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+Gst.init(None)
 
 #define some global variables
 this_dir = os.path.dirname( os.path.abspath(__file__) )
 
 
-class Recognizer(gobject.GObject):
+class Recognizer(GObject.GObject):
 	__gsignals__ = {
-		'finished' : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_STRING,))
+		'finished' : (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING,))
 	}
 	def __init__(self, language_file, dictionary_file, src = None):
-		gobject.GObject.__init__(self)
+		GObject.GObject.__init__(self)
 		self.commands = {}
 		if src:
 			audio_src = 'alsasrc device="hw:%d,0"' % (src)
@@ -26,32 +27,33 @@ class Recognizer(gobject.GObject):
 			audio_src = 'autoaudiosrc'
 
 		#build the pipeline
-		cmd = audio_src+' ! audioconvert ! audioresample ! vader name=vad ! pocketsphinx name=asr ! appsink sync=false'
+		cmd = audio_src+' ! audioconvert ! audioresample ! pocketsphinx name=asr ! appsink sync=false'
 		try:
-			self.pipeline=gst.parse_launch( cmd )
+			self.pipeline=Gst.parse_launch( cmd )
 		except Exception, e:
 			print e.message
 			print "You may need to install gstreamer0.10-pocketsphinx"
 			raise e
-
+		bus = self.pipeline.get_bus()
+		bus.add_signal_watch()
+		bus.connect('message::element', self.element_message)
 		#get the Auto Speech Recognition piece
 		asr=self.pipeline.get_by_name('asr')
-		asr.connect('result', self.result)
 		asr.set_property('lm', language_file)
 		asr.set_property('dict', dictionary_file)
 		asr.set_property('configured', True)
-		#get the Voice Activity DEtectoR
-		self.vad = self.pipeline.get_by_name('vad')
-		self.vad.set_property('auto-threshold',True)
+
+	def element_message(self, bus, msg):
+		"""Receive element messages from the bus."""
+		msgtype = msg.get_structure().get_name()
+		if msgtype != 'pocketsphinx':
+			return
+		if msg.get_structure().get_value('final'):
+			self.emit("finished", msg.get_structure().get_value('hypothesis'))
 
 	def listen(self):
-		self.pipeline.set_state(gst.STATE_PLAYING)
+		self.pipeline.set_state(Gst.State.PLAYING)
 
 	def pause(self):
-		self.vad.set_property('silent', True)
-		self.pipeline.set_state(gst.STATE_PAUSED)
-
-	def result(self, asr, text, uttid):
-		#emit finished
-		self.emit("finished", text)
+		self.pipeline.set_state(Gst.State.PAUSED)
 
